f6319bccdc10dce20c11f0450d5c4815515a0b0f
CASSE Enzo <enzocasse123@gmail.com>
2026-02-04 01:21:26 +0100

    feat: BackupJob and state model

---
+++ src/Core/Enums/BackupStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Enums
+{
+   public enum BackupStatus
+    {
+        Completed,
+        Error,
+        Active,
+        Inactive
+
+    }
+}
