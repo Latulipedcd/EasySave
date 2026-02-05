d98e8f92308e2314e61a0c52b43701b06e66b29a
Octave <octave.toulemonde@viacesi.fr>
2026-02-05 11:53:51 +0100

    Fix log writing bug causing missing entries

---
+++ src/Log/ILogWriter.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Log
+{
+    internal interface ILogWriter
+    {
+        void Write(Object entry);
+    }
+
+
+    class JsonLogWriter : ILogWriter
+    {
+        public void Write(Object entry)
+        {
+            string folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
+            string fileName = $"log-{DateTime.Now:yyyy-MM-dd}.json";
+            string path = Path.Combine(folder, fileName);
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            if (!File.Exists(path))
+            {
+                File.WriteAllText(path, "[]");
+            }
+
+            var json = File.ReadAllText(path);
+
+            JsonArray array = JsonNode.Parse(json)?.AsArray() ?? new JsonArray();
+
+            JsonNode newEntry = JsonSerializer.SerializeToNode(entry);
+
+            array.Add(newEntry);
+
+            File.WriteAllText(path, array.ToJsonString(new JsonSerializerOptions
+            {
+                WriteIndented = true
+            }));
+        }
+    }
+}
+
---
+++ src/Log/LogService.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection.Metadata;
+
+
+
+namespace Log
+{
+    public class LogService
+    {
+        private static LogService? _instance;
+
+        private ILogWriter writer;
+
+        public static LogService Instance
+        {
+            get
+            {
+
+                if (_instance == null)
+                {
+                    _instance = new LogService();
+                }
+                return _instance;
+            }
+        }
+
+        private LogService()
+        {
+            writer = new JsonLogWriter();
+        }
+
+        public void LogBackup(Object entry)
+        {
+            writer.Write(entry);
+        }
+
+
+    }
+}
