55d5435cae069bcfaa34c374106c6153cd51aae3
Octave <octave.toulemonde@viacesi.fr>
2026-02-24 17:01:56 +0100

    fix : catch exception if the server is offline

--- src/Core/Services/DockerLoggerService.cs
+++ src/Core/Services/DockerLoggerService.cs
@@ -50,19 +50,26 @@ namespace Core.Services
 
         public void SendLog(LogFormat format, LogEntry entry)
         {
-            if (!_isConnected) { Connect(); }
-
-            var logMessage = new Dictionary<string, object>
+            try
             {
-                { "Format", format },
-                { "Entry", entry }
-            };
+                if (!_isConnected) { Connect(); }
 
-            string json = JsonSerializer.Serialize(logMessage);
-            string message = json + "<|EOM|>";
-            byte[] data = Encoding.UTF8.GetBytes(message);
+                var logMessage = new Dictionary<string, object>
+                {
+                    { "Format", format },
+                    { "Entry", entry }
+                };
 
-            _socket.Send(data);
+                string json = JsonSerializer.Serialize(logMessage);
+                string message = json + "<|EOM|>";
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                _socket.Send(data);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         //public void Close()
